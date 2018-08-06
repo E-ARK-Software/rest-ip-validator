78e3acbb75f19b88bbe2d0206efebf3a8abba921
Carl Wilson <carl@openplanetsfoundation.org>
2018-08-06 11:23:44 +0100

    FEAT - basic HTTP error handling.

--- app-server/src/main/resources/assets/js/ip-validation.js
+++ app-server/src/main/resources/assets/js/ip-validation.js
@@ -16,10 +16,13 @@ var ipValidator = {
         ipValidator.result = jqXHR.responseText;
         callback();
       },
-      // Log call errors to the console
+      // HTTP Error handler
       error       : function (jqXHR, textStatus, errorThrown) {
+        // Log full error to console
         console.log('Validation Error: ' + textStatus + errorThrown);
         console.log(jqXHR);
+        // Alert the user with details
+        alert('Something has gone wrong!!\n\nHTTP ' + jqXHR.status + ': ' + jqXHR.statusText);
       }
     });
   }
