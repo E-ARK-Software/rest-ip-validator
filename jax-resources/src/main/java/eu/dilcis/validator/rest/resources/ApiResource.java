1728f5a4f9fb7bfa55fdfc427eb8e5894cf22dc8
Carl Wilson <carl@openplanetsfoundation.org>
2018-08-06 12:59:00 +0100

    FEAT - SHA-1 web service.

---
+++ jax-resources/src/main/java/eu/dilcis/validator/rest/resources/ApiResource.java
@@ -0,0 +1,31 @@
+package eu.dilcis.validator.rest.resources;
+
+import javax.ws.rs.Path;
+
+	
+/**
+ * @author  <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
+ *          <a href="https://github.com/carlwilson">carlwilson AT github</a>
+ *
+ * @version 0.1
+ */
+@Path("/")
+public final class ApiResource {
+
+	/**
+	 * 
+	 */
+	public ApiResource() {
+		super();
+	}
+
+
+    /**
+     * @return a new {@link ByteStreamResource}
+     */
+    @Path("/sha1")
+    public static ByteStreamResource getSha1() {
+        return new ByteStreamResource();
+    }
+
+}
