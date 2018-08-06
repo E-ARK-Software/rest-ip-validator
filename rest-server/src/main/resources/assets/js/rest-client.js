53db68698884c2cdbb23754821620d04aea62083
Carl Wilson <carl@openplanetsfoundation.org>
2018-08-06 08:20:01 +0100

    FEAT - file upload mechanism.

--- rest-server/src/main/resources/assets/js/rest-client.js
+++ rest-server/src/main/resources/assets/js/rest-client.js
@@ -1,11 +1,37 @@
+/**
+** Loaded up on start
+**/
 $(document).ready( function() {
-  console.log("ready");
+  // Event handler for the file selector
   $('input:file').on('change', function() {
+    // Disable the submit button
+    $('button').prop('disabled', true);
+    // Grab the label component
     var fileLabel = $(this).siblings('.custom-file-label');
-    var files = this.files;
-    var log = 'File selected: ' + $(this).val().split('\\').pop();
-    console.log(log);
-    fileLabel.val($(this).val());
-    $("#digest").val(digest);
+    // Get filename without the fake path prefix
+    var fileName = $(this).val().split('\\').pop();
+    // Set the filename selection, a little tricksy
+    fileLabel.addClass("selected").html(fileName);
+    // Calculate and display the SHA1 of the file
+    calcFileSha1(this.files[0]);
   });
 });
+
+function calcFileSha1(file) {
+  // New checksum calculator instance
+  rusha = new Rusha();
+  // File reader to get data
+  reader = new FileReader();
+  // Register reader onload event
+  reader.onload = function(e) {
+    // Calculate the checksum from the reader result
+    var digest = rusha.digest(reader.result);
+    // Set the label when finished
+    $("#digest").val(digest);
+    // Enable the submit button
+    $('button').prop('disabled', false);
+  }
+  // Signal checkcum calculation and load reader
+  $("#digest").val("Calcluating package checksum...");
+  reader.readAsBinaryString(file);
+}
